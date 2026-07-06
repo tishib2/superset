84c212106fe09056b7a77c3497d83bb46736f5e0
tishib <itakaki0@gmail.com>
2026-07-06 10:33:29 +0900

    test: trigger flatten-tests workflow

--- superset-frontend/src/visualizations/TimeTable/utils/sortUtils/sortUtils.test.ts
+++ superset-frontend/src/visualizations/TimeTable/utils/sortUtils/sortUtils.test.ts
@@ -1,4 +1,4 @@
-// trigger: flatten-tests demo v33
+// trigger: flatten-tests demo vNEXT
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
